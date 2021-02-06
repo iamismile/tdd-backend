a77d2a2ca16f21bd84f00f74bde769f587cfb187
Ismile <iamismile.dev@gmail.com>
2021-02-06 07:34:53 +0600

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const User = require('./user/User');
-const bcrypt = require('bcrypt');
-
+const UserRouter = require('./user/UserRouter');
 const app = express();
 
 app.use(express.json());
-
-app.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    // const user = Object.assign({}, req.body, { password: hash });
-    const user = { ...req.body, password: hash };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
-});
+app.use(UserRouter);
 
 module.exports = app;
