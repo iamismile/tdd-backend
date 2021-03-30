03633d7a82a802926cb92e45874b4319f1c01e68
Ismile Hossain <iamismile.dev@gmail.com>
2021-03-30 07:12:30 +0600

    Page content

--- __test__/UserListing.spec.js
+++ __test__/UserListing.spec.js
@@ -1,5 +1,32 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(async () => {
+  // Create tables for all defined models to the DB if it doesn't exist
+  // and does nothing if it already exists
+  await sequelize.sync();
+});
+
+beforeEach(async () => {
+  // To destroy everything in the table
+  await User.destroy({ truncate: true });
+});
+
+const getUsers = async () => {
+  return await request(app).get('/api/1.0/users');
+};
+
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
+      inactive: i >= activeUserCount,
+    });
+  }
+};
 
 describe('Listing Users', () => {
   it('returns 200 ok when there is no user in database', async () => {
@@ -8,12 +35,31 @@ describe('Listing Users', () => {
   });
 
   it('returns page object as response body', async () => {
-    const response = await request(app).get('/api/1.0/users');
+    const response = await getUsers();
     expect(response.body).toEqual({
       content: [],
       page: 0,
       size: 10,
       totalPages: 0,
     });
+  });
+
+  it('returns 10 users in page content when there are 11 users in database', async () => {
+    await addUsers(11);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(10);
+  });
+
+  it('returns 6 user in page content when there are active 6 users and inactive 5 users in database', async () => {
+    await addUsers(6, 5);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(6);
+  });
+
+  it('returns only id, username and email in content array for each user', async () => {
+    await addUsers(11);
+    const response = await getUsers();
+    const user = response.body.content[0];
+    expect(Object.keys(user)).toEqual(['id', 'username', 'email']);
   });
 });
