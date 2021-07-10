15a428478e801e77879b5561892716ab5457270f
Ismile Hossain <iamismile.dev@gmail.com>
2021-07-10 16:48:57 +0600

    Cleaning up the folders

--- __test__/UserUpdate.spec.js
+++ __test__/UserUpdate.spec.js
@@ -23,14 +23,6 @@ beforeEach(async () => {
   await User.destroy({ truncate: { cascade: true } });
 });
 
-afterAll(() => {
-  // Clean uploads directory
-  const files = fs.readdirSync(profileDirectory);
-  for (const file of files) {
-    fs.unlinkSync(path.join(profileDirectory, file));
-  }
-});
-
 const activeUser = {
   username: 'user1',
   email: 'user1@mail.com',
